2ebf362b2e69fa9801a49d253237c12df694fdb8
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-04 21:56:35 +0545

    i don't know

---
+++ 23_Recursion II/Q11.java
@@ -0,0 +1,15 @@
+public class Q11
+{
+    public static int tilingproblem(int n)
+    {
+        if(n==1 || n==0) return 1;
+        int fnm1 = tilingproblem(n-1);
+        int fnm2= tilingproblem(n-2);
+        int noofWays= fnm1+fnm2;
+        return noofWays;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingproblem(4));
+    }
+}
