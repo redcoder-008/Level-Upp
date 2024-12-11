289b40bd70e4a1d03fe32b1a7cb6922d45f8d6ae
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-11 16:45:14 +0545

    TIME AND SPACE

---
+++ 25_TimeAndSpace/examples.java
@@ -0,0 +1,13 @@
+public class examples{
+    public static void main(String[] args) {
+         /* //simple loop
+        for(int i=0;i<n;i++)
+        {
+            //some constant work is done here
+        }
+            TC: O(n)
+            */ 
+            
+
+    }
+}
