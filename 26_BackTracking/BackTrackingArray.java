be398b6959527adeb1f089ea8060a07d0c1c67bf
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-13 09:53:32 +0545

    change nothing

--- 26_BackTracking/BackTrackingArray.java
+++ 26_BackTracking/BackTrackingArray.java
@@ -1,6 +1,23 @@
 public class BackTrackingArray{
-    public static void  
+    public static void  bcktrack(int arr[],int i,int val)
+    {
+        if(i==arr.length)
+        {
+            return;
+        }
+        arr[i]= val;
+        bcktrack(arr, i+1, 1);
+        arr[i]= arr[i]-2;
+    }
     public static void main(String[] args) {
+        int arr[]= new int [5];
+        bcktrack(arr, 0, 1);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]);
+            System.out.print(arr[i]);
+            
+        }
         
     }
 }
