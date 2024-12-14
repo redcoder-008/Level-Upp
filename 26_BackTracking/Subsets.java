5ad0c1a597ef191db304eebaa0072e1a348d07a1
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-14 10:17:00 +0545

    Find Subsets

---
+++ 26_BackTracking/Subsets.java
@@ -0,0 +1,21 @@
+public class Subsets {
+    public static void printSubsets(String str,String ans,int i)
+    {
+        if(i==str.length())
+        {
+            if(ans.length()==0) System.out.println("NULL");
+            else{
+                System.out.println(ans);
+                
+            }
+            return;
+        }
+        //if yes
+        printSubsets(str, ans+str.charAt(i), i+1);
+        //if No 
+        printSubsets(str, ans, i+1);
+    }public static void main(String[] args) {
+        String str = "XYZ";
+        printSubsets(str, "", 0);
+    }
+}
