88c13ceb0c0901f09488dbfc83ac4b6ce8f9f151
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-14 10:50:00 +0545

    Find Permutation

---
+++ 26_BackTracking/Permutation.java
@@ -0,0 +1,21 @@
+public class Permutation {
+    public static void FindPermutation(String str,String ans)
+    {
+        if(str.length()==0) 
+        {
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+            char currcar= str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            FindPermutation(newStr, ans+ currcar);
+
+        }
+    }
+    public static void main(String[] args) {
+        String str= "abc";
+        FindPermutation(str, "");
+    }
+}
