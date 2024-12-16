ef3d48b9ce6dc0c462675e8c1d5014fe36350b4f
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-16 19:10:08 +0545

    Place the N Queen in the N size of Chess Board

---
+++ 26_BackTracking/NQueens.java
@@ -0,0 +1,68 @@
+public class NQueens {
+    public static void PlaceQueen(String board[][], int row, int n)
+    {
+        if(row==n)
+        {
+            printBoard(board, n);
+            return;
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(isSafe(board,row,j,n)) //
+            {
+                board[row][j] = "Q";
+                PlaceQueen(board,row+1,n);
+                board[row][j]= "X";
+            }
+        }
+    }
+    public static boolean isSafe(String board[][],int row,int col,int n){
+        //horizotally
+        for(int i=0;i<n;i++)
+        {
+            if(board[row] [i] =="Q") return false;
+        }
+        //vertically up 
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == "Q" ) return false;
+        }
+        //left diagonally up 
+        for(int i= row-1,j=col-1 ;i>=0 && j>=0;i--,j-- )
+        {
+            if(board[i][j] == "Q") return false;
+        }
+        //right diagonally up
+        for(int i=row-1,j=col+1;i>=0 && j<n; i--,j++)
+        {
+            if(board[i][j] == "Q") return false;
+        }
+        return true;
+    }
+    
+    public static void printBoard( String  board [] [],int n)
+    {
+        System.out.println("------Chess Board-------");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int n=4;
+        String board[][]= new String[n][n] ;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                board[i][j]= "x";
+            }
+        }
+        PlaceQueen(board, 0, n);
+       
+    }
+}
