3f2eb0c2dc7e838c14f70cfd62b0ea66b7c7f681
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-16 21:40:57 +0545

    total strings in permutaion

--- 26_BackTracking/Permutation.java
+++ 26_BackTracking/Permutation.java
@@ -7,6 +7,7 @@ public class Permutation {
             return ;
         }
         for(int i=0;i<str.length();i++)
+        //
         {
             char currcar= str.charAt(i);
             String newStr = str.substring(0,i) + str.substring(i+1);
