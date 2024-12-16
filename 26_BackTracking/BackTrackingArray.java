b3d1e7ca283447117098359ef9ebf7f4ba5411d5
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-16 17:56:43 +0545

    change

--- 26_BackTracking/BackTrackingArray.java
+++ 26_BackTracking/BackTrackingArray.java
@@ -15,7 +15,7 @@ public class BackTrackingArray{
         for(int i=0;i<arr.length;i++)
         {
             System.out.print(arr[i]);
-            System.out.print(arr[i]);
+            
             
         }
         
