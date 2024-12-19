75be8e466d590340b5242da4af7793e05383005c
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-19 15:22:27 +0545

    check if the cycle is formed or not??

--- Linked List P1/linkedListP2.java
+++ Linked List P1/linkedListP2.java
@@ -1,7 +1,40 @@
+import java.lang.classfile.components.ClassPrinter.Node;
+
 public class linkedListP2 {
-    public class node{
+    public  static class Node{
         int data ;
+        Node next;
+        public Node (int data){
+            this.data = data;
+            this.next=null;
+        }
         
+    }
+    public static boolean isCycle(){
+        Node slow= head;
+        Node fast= head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true; //it means that is linked list is forming cylcle
+            }
+        }
+        return false ;// no cycle is formed
+    }
+    public static Node head;
+    public static Node size;
+    
+    public static void main(String[] args) {
+      
+        head = new Node (1);
+        head.next = new Node(2);
+        head.next.next= new  Node(3);
+        head.next.next.next =head; 
+        System.out.println(isCycle());      
+
     }
     
 }
