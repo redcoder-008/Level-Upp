e76b8bcf1d2f6c330933b29003872266f40dfd8f
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-20 15:36:09 +0545

    swapping and reversing of arraylist

--- 27_ArrayList/arr.java
+++ 27_ArrayList/arr.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class arr{
     public static int maxElement(ArrayList <Integer> list){
         int max= Integer.MIN_VALUE;
@@ -9,18 +10,32 @@ public class arr{
         }
         return max;
     }
+    public static void swap(ArrayList<Integer> list ,int idx1,int idx2){
+        int temp= list.get(idx1);
+        list.set( idx1,list.get(idx2));
+        list.set(idx2,temp); 
+    }
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public static void main(String [] args){
         ArrayList list = new ArrayList<>();
-        list.add(1);
+        list.add(11);
         list.add(2);
-        list.add(3);
-        list.add(4);
-        for(int i=0;i<list.size();i++)
-        {
-            System.out.print(list.get(i)+" ");
-        }
-        System.out.println();
-        System.out.println(maxElement(list));
+        list.add(31);
+        list.add(12);
+      
+          System.out.println(list);
+          /*  
+          
+          System.out.println(maxElement(list));
+          swap(list,1,3);
+          System.out.println(list);
+          */   
+          Collections.sort(list); //sorting in ascending order
+          System.out.println(list);
+          Collections.sort(list, Collections.reverseOrder());//sorting in an descending order
+          
+          System.out.println(list);
 
 
 
