bb8fb9157836072310212a189c7f9cc3821b0192
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-20 15:10:46 +0545

    max element

--- 27_ArrayList/arr.java
+++ 27_ArrayList/arr.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
 public class arr{
+    public static int maxElement(ArrayList list){
+        int max= Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if( max < (int)list.get(i)){
+                max= (int) list.get(i);
+            }
+        }
+        return max;
+    }
     public static void main(String [] args){
-        
+        ArrayList list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+        System.out.println(maxElement(list));
+
+
+
+
 
     }
 }
