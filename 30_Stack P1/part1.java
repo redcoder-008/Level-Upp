8b896f8126db8b5520a3378049b5ce4707169067
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-21 19:04:15 +0545

    Stack

--- 30_Stack P1/part1.java
+++ 30_Stack P1/part1.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Stack;
 public class part1{
-    static class stack{
+    static class Stack{
         static ArrayList<Integer> list = new ArrayList<>();
         public static boolean isEmpty(){
             return (list.size()==0);
@@ -10,7 +11,30 @@ public class part1{
             list.add(data);
             return;
         }
+        //pop data
+        public static int pop(){
+            int top= list.get(list.size()-1);
+            list.remove ( list.size()-1);
+            return top;
+        }
+        //peek data 
+        public static int peek(){
+            int top= list.get(list.size()-1);
+            return top;
+        }
 
 
+    }
+    public static void main(String[] args) {
+        Stack s = new  Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while( !s.isEmpty() ){
+            System.out.println(s.peek());
+            s.pop();
+        }
+        
     }
 }
