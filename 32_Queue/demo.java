8de4c424321904588dc97dc310c9d133a337fe76
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2024-12-29 21:22:18 +0545

    leetcode no 1209 code

--- 32_Queue/demo.java
+++ 32_Queue/demo.java
@@ -1,3 +1,64 @@
-public class demo {
+import java.util.Stack;
+
+class Pair
+{
+     char chr;
+     int freq;
     
+     Pair( char chr, int freq)
+     {
+          this.chr= chr;
+          this.freq= freq;
+     }
 }
+
+class Solution {
+    public String removeDuplicates(String s, int k)
+    {
+        Stack<Pair>st= new Stack<>();
+        
+        for( int i=0;i< s.length();i++)
+        {
+             char ch = s.charAt(i);
+            
+            if( st.isEmpty()==false && st.peek().chr == ch )
+            {
+                 st.peek().freq++;
+                 
+                 if( st.peek().freq == k )
+                 {
+                     st.pop();
+                 }
+            }
+            else
+            {
+                Pair p = new Pair( ch, 1);
+                
+                st.push( p );
+            }
+        }
+        
+        
+        StringBuilder ans= new StringBuilder();
+        
+        while( st.isEmpty()==false)
+        {
+            int freq= st.peek().freq;
+            char ch = st.peek().chr;
+            st.pop();
+            
+            for( int i=1;i<= freq;i++)
+            {
+                 ans.append( ch );
+            }
+        }
+        
+        ans.reverse();
+        return ans.toString();
+    }
+}
+
+
+
+
+
