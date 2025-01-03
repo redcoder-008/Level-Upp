dcc580e029036adbc2aec100b6e3f7ba8281b9b5
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-01-03 17:17:43 +0545

    Indian Coins code

---
+++ Greedy Algorithm/indianCoin.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class indianCoin {
+    public static void main(String[] args) {
+        Integer coin[]={2000,500,100,50,20,10,5,2,1};
+        Arrays.sort(coin, Comparator.reverseOrder());
+        int amount = 590;
+        int money=0;
+        ArrayList <Integer> ans = new ArrayList<>();
+        for(int i=0;i<coin.length;i++){
+            if(coin[i] <= amount){
+                while(coin[i] <= amount){
+                    amount -= coin[i];
+                    ans.add(coin[i]);
+                
+                    money++;
+                }
+            }
+        }
+        System.out.println("YOu need to give " + money+ " Coins ");
+      for(int i=0;i<ans.size();i++){
+        System.out.print(ans.get(i) +" ");
+
+      }
+    }
+}
