f2146f3ad6d7c1074d12826d288389535e30ab06
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-01-04 15:31:48 +0545

    po

--- 34_BinaryTree P1/BuildTreePreOrder.java
+++ 34_BinaryTree P1/BuildTreePreOrder.java
@@ -43,12 +43,21 @@ public class BuildTreePreOrder{
         System.out.print(root.data+" ");
         inOrder(root.right);
     }
+//post order
+public static void postOrder(Node root){
+    if(root == null){
+        return;
+    }
+    postOrder(root.left);
+    postOrder(root.right);
+    System.out.print(root.data+" ");
+} 
     @SuppressWarnings("static-access")
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root= tree.BuildTree(nodes);
        // System.out.println(root.data);
-       inOrder(root);
+       postOrder(root);
     }
 }
