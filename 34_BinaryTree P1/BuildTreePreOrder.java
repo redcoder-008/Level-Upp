d9a19f8bb71c02447dbf8788c9565bbcceb28168
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-01-04 15:42:25 +0545

    lo

--- 34_BinaryTree P1/BuildTreePreOrder.java
+++ 34_BinaryTree P1/BuildTreePreOrder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BuildTreePreOrder{
   static  class Node{
@@ -52,12 +55,41 @@ public static void postOrder(Node root){
     postOrder(root.right);
     System.out.print(root.data+" ");
 } 
+//level order
+public static void levelOrder(Node root){
+    if(root==null) return;
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    q.add(null);
+    while(!q.isEmpty()){
+        Node currNode = q.remove();
+        if(currNode==null){
+            System.out.println();
+            if(q.isEmpty()){
+
+                break;
+            } else{
+                q.add(null);
+            }
+
+        }else{ 
+        System.out.print(currNode.data+" ");
+        if(currNode.left != null) {
+            q.add(currNode.left);
+        }
+        if(currNode.right != null){
+            q.add(currNode.right);
+        }
+    }
+    }
+
+}
     @SuppressWarnings("static-access")
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root= tree.BuildTree(nodes);
        // System.out.println(root.data);
-       postOrder(root);
+       levelOrder(root);
     }
 }
