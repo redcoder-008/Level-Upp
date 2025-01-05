d34e0c0186a54e1da5a0651e031bdaa4c3b04f39
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-01-05 16:53:55 +0545

    Height Of a tree Code!!!!

---
+++ 34_BinaryTree P1/heightOfATree.java
@@ -0,0 +1,30 @@
+class Node{
+    int data;
+    Node left,right;
+    Node(int data ){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class heightOfATree {
+    public static int findHeight(Node root){
+        if(root==null) return 0;
+        int lh= findHeight(root.left);
+        int rh=findHeight(root.right);
+        return Math.max(lh,rh)+1;
+    }
+    public static void main(String[] args) {
+        Node root= new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left= new Node(4);
+        root.left.right= new Node(5);
+       
+        root.right.left= new Node(6);
+        root.right.right= new Node(7);
+        System.out.println(findHeight(root));
+
+    }
+    
+}
