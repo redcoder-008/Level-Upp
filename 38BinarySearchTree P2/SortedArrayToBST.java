b2ea0879c9eecaf34ca36cc692db3174c161af34
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-04-25 20:03:42 +0545

    Sorted Array To Balanced BST

--- 38BinarySearchTree P2/SortedArrayToBST.java
+++ 38BinarySearchTree P2/SortedArrayToBST.java
@@ -1,24 +1,37 @@
 public class SortedArrayToBST{
-    class Node {
+ static  class Node {
         int data;
-        Node root;
+
         Node left;
         Node right;
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+        Node(int val){
+            this.data=val;
+            this.left = this.right = null;
+            
         }
     }
+    public static void preorder(Node root){
+        if(root == null) return;
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
 
-    public static Node createBSt(int arr,int st,int end){
+
+    public static Node createBSt(int arr[],int st,int end){
         if(st>end) return null;
         int mid= (st+end)/2;
-        Node root= New Node (arr[mid]);
+        Node root= new Node (arr[mid]);
         root.left = createBSt(arr, st,mid-1);
         root.right= createBSt(arr,mid+1,end);
         return root;
 
+    }
+    public static void main(String[] args) {
+        int arr[]= {3,5,6,8,10,11,12};
+      Node root=  createBSt(arr, 0,arr.length-1);
+      
+      preorder(root);
     }
 
 }
