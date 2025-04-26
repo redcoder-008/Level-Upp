fec8a704c0705b3cde378b3756475b06a14e2801
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-04-26 18:26:41 +0545

    Size of largest Binary Sorted Tree in Binary Tree

--- 38BinarySearchTree P2/SizeOFLargestBSTInBT.java
+++ 38BinarySearchTree P2/SizeOFLargestBSTInBT.java
@@ -9,6 +9,18 @@ public class SizeOFLargestBSTInBT {
             this.isBst = isBst;
             this.min = min;
             this.max = max;
+            this.size =size;
+        }
+    }
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int val) {
+            this.data = val;
+            this.left = this.right = null;
         }
     }
 
@@ -17,8 +29,7 @@ public class SizeOFLargestBSTInBT {
     public static Info largestBSt(Node root) {
         if (root == null) {
             return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
-
-        }
+                         }
         Info leftInfo = largestBSt(root.left);
         Info rightInfo = largestBSt(root.right);
         int size = leftInfo.size + rightInfo.size + 1;
@@ -26,13 +37,40 @@ public class SizeOFLargestBSTInBT {
         int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
         if (root.data <= leftInfo.max || root.data >= rightInfo.min) {
             return new Info(false, size, min, max);
-
         }
+
         if (leftInfo.isBst && rightInfo.isBst) {
             maxBST = Math.max(maxBST, size);
-            new Info(true, size, min, max);
-
+            return new Info(true, size, min, max);
+            
         }
+        return new Info(false, size, min, max);
 
     }
+    public static void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+        
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.left.left = new Node(5);
+        root.left.right = new Node(20);
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
+        inorder(root);
+        System.out.println();
+        Info info = largestBSt(root);
+        System.out.println("The size of the largest BST in BT is "+ maxBST);
+        System.out.println(maxBST);
+
+    }
+
 }
