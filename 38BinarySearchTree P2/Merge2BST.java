46fdf8d9616ac739670d6b8a0e4ebcec209d0911
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-04-26 19:15:03 +0545

    merge

--- 38BinarySearchTree P2/Merge2BST.java
+++ 38BinarySearchTree P2/Merge2BST.java
@@ -11,19 +11,43 @@ public class Merge2BST {
             this.left = this.right = null;
         }
     }
-    public static void inorder(Node root,ArrayList<Integer> array){
-        if(root==null) return;
+
+    public static void inorder(Node root, ArrayList<Integer> array) {
+        if (root == null)
+            return;
         inorder(root.left, array);
         array.add(root.data);
         inorder(root.right, array);
     }
-    public static Node merge(Node root1,Node root2){
-        ArrayList<Integer>arr1 = new ArrayList<>();
-        inorder(root1,arr1);
-        ArrayList<Integer>arr2 = new ArrayList<>();
-        inorder(root2,arr2);
-        //merge
-        
+
+    public static Node merge(Node root1, Node root2) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        inorder(root1, arr1);
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        inorder(root2, arr2);
+        // merge
+        ArrayList<Integer> finalarr = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < arr1.size() && j < arr2.size()) {
+            if (arr1.get(i) < arr2.get(j)) {
+                finalarr.add(arr1.get(i));
+                i++;
+            } else {
+                finalarr.add(arr2.get(j));
+                j++;
+            }
+        }
+        while(i<arr1.size()){
+            finalarr.add(arr1.get(i));
+            i++;
+
+        }
+        while(j<arr2.size()){
+            finalarr.add(arr2.get(j));
+            j++;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -33,8 +57,8 @@ public class Merge2BST {
         Node root2 = new Node(9);
         root2.left = new Node(3);
         root2.right = new Node(12);
-merge(root1, root2);
-System.out.println(Array.get(1));
+        merge(root1, root2);
+        System.out.println(Array.get(1));
     }
 
 }
