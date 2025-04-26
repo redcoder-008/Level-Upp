4fbbb1742b479038b6e568e198873b76a7460d39
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-04-26 17:53:31 +0545

    format code

--- 38BinarySearchTree P2/SortedArrayToBST.java
+++ 38BinarySearchTree P2/SortedArrayToBST.java
@@ -1,37 +1,41 @@
-public class SortedArrayToBST{
- static  class Node {
+public class SortedArrayToBST {
+    static class Node {
         int data;
 
         Node left;
         Node right;
-        Node(int val){
-            this.data=val;
+
+        Node(int val) {
+            this.data = val;
             this.left = this.right = null;
-            
+
         }
     }
-    public static void preorder(Node root){
-        if(root == null) return;
-        System.out.print(root.data+" ");
+
+    public static void preorder(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.data + " ");
         preorder(root.left);
         preorder(root.right);
     }
 
-
-    public static Node createBSt(int arr[],int st,int end){
-        if(st>end) return null;
-        int mid= (st+end)/2;
-        Node root= new Node (arr[mid]);
-        root.left = createBSt(arr, st,mid-1);
-        root.right= createBSt(arr,mid+1,end);
+    public static Node createBSt(int arr[], int st, int end) {
+        if (st > end)
+            return null;
+        int mid = (st + end) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = createBSt(arr, st, mid - 1);
+        root.right = createBSt(arr, mid + 1, end);
         return root;
 
     }
+
     public static void main(String[] args) {
-        int arr[]= {3,5,6,8,10,11,12};
-      Node root=  createBSt(arr, 0,arr.length-1);
-      
-      preorder(root);
+        int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
+        Node root = createBSt(arr, 0, arr.length - 1);
+
+        preorder(root);
     }
 
 }
