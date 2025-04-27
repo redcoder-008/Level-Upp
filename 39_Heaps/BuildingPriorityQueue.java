1943b6dc936cac400312059f040ac9ba4ec18f09
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-04-27 09:04:25 +0545

    BuildingPriorityQueue

--- 39_Heaps/BuildingPriorityQueue.java
+++ 39_Heaps/BuildingPriorityQueue.java
@@ -1,19 +1,21 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
+
 public class BuildingPriorityQueue {
     public static void main(String[] args) {
         // Example usage of the BuildingPriorityQueue class
-        PriorityQueue<Integer>pq= new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // for the reverse order
         pq.add(3);
         pq.add(5);
         pq.add(1);
         pq.add(9);
         pq.add(6);
         pq.add(7);
-        while(!pq.isEmpty()){
-            System.out.print(pq.peek()+" ");
+        while (!pq.isEmpty()) {
+            System.out.print(pq.peek() + " ");
             pq.remove();
         }
-
 
     }
 }
