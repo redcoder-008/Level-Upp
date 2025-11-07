ccd7003351982bbc6d56913b2ac07c694a2f7542
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-11-07 19:05:47 +0545

    journey begins again

--- 39_Heaps/InsertInHeap.java
+++ 39_Heaps/InsertInHeap.java
@@ -1,23 +1,28 @@
 import java.util.ArrayList;
 
 public class InsertInHeap {
-   public static class Heap{
-        ArrayList<Integer>arr = new ArrayList<>();
-        public void add(int data){
-            //add at the last
-            arr.add(data);
-            int x= arr.size()-1;
-            int par= (x-1)/2; //parent index
-            int temp = arr.get(x);
-            arr.set(x,arr.get(par));
-            arr.set(par,temp);
+    public static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
 
+        public void add(int data) {
+            // add at the last
+            arr.add(data);
+            int x = arr.size() - 1;
+            int par = (x - 1) / 2; // parent index
+            while (arr.get(x) < arr.get(par)) {
+
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+
+            }
 
         }
 
     }
+
     public static void main(String[] args) {
-    
+
     }
-    
+
 }
